ef2f5da264da658fc7e9ea83c071d833908b4136
Gleason <bianliuzhu@gmail.com>
2022-02-13 13:16:29 +0800

    feat: 初始化css样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*
+ * @Description: 主组建
+ * @Author: Gleason
+ * @Date: 2022-02-13 10:51:54
+ * @LastEditors: Gleason
+ * @LastEditTime: 2022-02-13 13:11:11
+ */
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>2werwerewr</h1>
+				<h2>ssadfafdsasd</h2>
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+ * @Description: 入口文件
+ * @Author: Gleason
+ * @Date: 2022-02-13 10:51:54
+ * @LastEditors: Gleason
+ * @LastEditTime: 2022-02-13 12:49:31
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.sass";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
