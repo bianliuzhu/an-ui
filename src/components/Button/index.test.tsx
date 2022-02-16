1f5efb7912e73f8e92d77c30b7f84b698f6368b7
Gleason <bianliuzhu@gmail.com>
2022-02-16 23:29:10 +0800

    feat: 添加button测试

--- src/components/Button/index.test.tsx
+++ src/components/Button/index.test.tsx
@@ -3,7 +3,59 @@
  * @Author: Gleason
  * @Date: 2022-02-13 11:29:33
  * @LastEditors: Gleason
- * @LastEditTime: 2022-02-13 11:55:04
+ * @LastEditTime: 2022-02-16 23:28:00
  */
+import { render, screen, fireEvent } from "@testing-library/react";
+import Button, { ButtonType, ButtonSize, ButtonProps } from ".";
 
-export {};
+const defaultProps = {
+	onClick: jest.fn(),
+};
+const testProps: ButtonProps = {
+	btnType: ButtonType.Primary,
+	size: ButtonSize.Large,
+	className: "kclass",
+};
+const disabledProps: ButtonProps = {
+	disabled: true,
+	onClick: jest.fn(),
+};
+describe("button 组件测试", () => {
+	it("默认测试", () => {
+		render(<Button {...defaultProps}>nice</Button>);
+		const element = screen.getByText("nice") as HTMLButtonElement;
+		// 断言 存在 button 标签
+		expect(element.tagName).toEqual("BUTTON");
+		// 断言 存在 默认 class 【btn btn-default】
+		expect(element).toHaveClass("btn btn-default");
+		expect(element.disabled).toBeFalsy();
+		// 模拟 点击
+		fireEvent.click(element);
+		// 断言 已经被调用
+		expect(defaultProps.onClick).toHaveBeenCalled();
+	});
+	it("根据不同 props 显示样式", () => {
+		render(<Button {...testProps}>nice</Button>);
+		const element = screen.getByText("nice");
+		expect(element).toHaveClass("btn-primary btn-lg kclass");
+	});
+	it("a连接状态", () => {
+		render(
+			<Button btnType={ButtonType.Link} href="www.baidu.com">
+				link
+			</Button>
+		);
+		const element = screen.getByText("link");
+		expect(element.tagName).toEqual("A");
+		expect(element).toBeInTheDocument();
+		expect(element).toHaveClass("btn btn-link");
+	});
+	it("禁用测试", () => {
+		render(<Button {...disabledProps}>nice</Button>);
+		const element = screen.getByText("nice") as HTMLButtonElement;
+		expect(element.disabled).toBeTruthy();
+		fireEvent.click(element);
+		// 没有被调用
+		expect(disabledProps.onClick).not.toHaveBeenCalled();
+	});
+});
