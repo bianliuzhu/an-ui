59c97aea30fb7572b8f804d9fd14c485e56bec20
Gleason <bianliuzhu@gmail.com>
2022-02-23 22:33:36 +0800

    feat: commit lint 配置

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Description:
+ * @Author: Gleason
+ * @Date: 2022-02-23 22:31:37
+ * @LastEditors: Gleason
+ * @LastEditTime: 2022-02-23 22:31:38
+ */
+module.exports = {
+	extends: ["@commitlint/config-conventional"],
+};
