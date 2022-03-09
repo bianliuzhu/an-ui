f4f67534958431276cc0c9a20683d24c6df79fa7
Gleason <bianliuzhu@gmail.com>
2022-03-09 21:31:34 +0800

    feat: 配置构建ts.json

--- src/components/icon/icon.stories.tsx
+++ src/components/icon/icon.stories.tsx
@@ -3,12 +3,12 @@
  * @Author: Gleason
  * @Date: 2022-03-06 21:30:55
  * @LastEditors: Gleason
- * @LastEditTime: 2022-03-06 22:36:37
+ * @LastEditTime: 2022-03-09 21:26:14
  */
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import Icon from "./index";
+import Icon from "./Icon";
 
 export default {
 	title: "图标",
