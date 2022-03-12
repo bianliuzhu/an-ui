4899b56e41a650363cac9aed772c6fc9d53b7d72
Gleason <bianliuzhu@gmail.com>
2022-03-12 21:43:47 +0800

    feat: 添加打包Icon引入

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,14 @@
  * @Author: Gleason
  * @Date: 2022-02-13 10:51:54
  * @LastEditors: Gleason
- * @LastEditTime: 2022-03-09 21:03:24
+ * @LastEditTime: 2022-03-12 21:42:00
  */
+// 引入 Icon
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fas);
+
+// 导出组件
 export { default as Button } from "./components/button";
 export { default as Icon } from "./components/icon";
 export { default as Menu } from "./components/menu";
