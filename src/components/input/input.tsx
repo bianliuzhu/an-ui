bed7d157fdd9bbc4b36d155f8e82ab3a07e3ab79
Gleason <bianliuzhu@gmail.com>
2022-03-28 22:11:32 +0800

    feat: input 组件基本结构构建完成

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -3,5 +3,23 @@
  * @Author: Gleason
  * @Date: 2022-03-28 21:41:35
  * @LastEditors: Gleason
- * @LastEditTime: 2022-03-28 21:51:08
+ * @LastEditTime: 2022-03-28 22:10:57
  */
+import { FC, ReactElement, InputHTMLAttributes } from "react";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+type InputSize = "large" | "small";
+type inputPand = string | ReactElement;
+export interface InputProps
+	extends Omit<InputHTMLAttributes<HTMLElement>, "size"> {
+	disabled?: boolean;
+	size?: InputSize;
+	icon?: IconProp;
+	prepand?: inputPand;
+	append?: inputPand;
+}
+
+const Input: FC<InputProps> = (props) => {
+	return <input type="text" />;
+};
+
+export default Input;
