c2b904376a42f23097d8084a163b490877a590ac
Gleason <bianliuzhu@gmail.com>
2022-03-29 22:12:44 +0800

    feat: 定义input css文件及input具体实现

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -3,10 +3,12 @@
  * @Author: Gleason
  * @Date: 2022-03-28 21:41:35
  * @LastEditors: Gleason
- * @LastEditTime: 2022-03-28 22:10:57
+ * @LastEditTime: 2022-03-29 22:10:23
  */
 import { FC, ReactElement, InputHTMLAttributes } from "react";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import classNames from "classnames";
+import Icon from "../icon";
 type InputSize = "large" | "small";
 type inputPand = string | ReactElement;
 export interface InputProps
@@ -14,12 +16,34 @@ export interface InputProps
 	disabled?: boolean;
 	size?: InputSize;
 	icon?: IconProp;
-	prepand?: inputPand;
+	prepend?: inputPand;
 	append?: inputPand;
 }
 
 const Input: FC<InputProps> = (props) => {
-	return <input type="text" />;
+	const { disabled, size, icon, prepend, append, style, ...restProps } = props;
+	// 取出各种属性
+	// 根据属性计算 className
+	const cnames = classNames("an-input-wrapper", {
+		[`input-size-${size}`]: size,
+		"is-disabled": disabled,
+		"input-group": prepend || append,
+		"input-group-append": !!append,
+		"input-group-prepend": !!prepend,
+	});
+	// 根据属性计算是否添加特定的节点
+	return (
+		<div className={cnames} style={style}>
+			{prepend && <div className="an-input-group-prepend">{prepend}</div>}
+			{icon && (
+				<div className="icon-wrapper">
+					<Icon icon={icon} title={`title-${icon}`} />
+				</div>
+			)}
+			<input className="an-input-inner" disabled={disabled} {...restProps} />
+			{append && <div className="an-input-group-append">{append}</div>}
+		</div>
+	);
 };
 
 export default Input;
